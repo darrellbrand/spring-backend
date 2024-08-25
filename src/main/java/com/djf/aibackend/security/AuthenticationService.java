b376d6c971d5da2bd3a66484062757173f431c93
dj <darrellbrand@gmail.com>
2024-08-25 16:14:56 -0700

    fix key

--- src/main/java/com/djf/aibackend/security/AuthenticationService.java
+++ src/main/java/com/djf/aibackend/security/AuthenticationService.java
@@ -1,22 +1,22 @@
 package com.djf.aibackend.security;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthenticationService {
 
     private static final String AUTH_TOKEN_HEADER_NAME = "X-API-KEY";
-    private static final String AUTH_TOKEN = "DarrellIsTheShit";
-
     public static Authentication getAuthentication(HttpServletRequest request) {
         String apiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
-        if (apiKey == null || !apiKey.equals(AUTH_TOKEN)) {
+        if (apiKey == null || !apiKey.equals(ApiKeyHolder.getLocalKey())) {
             throw new BadCredentialsException("Invalid API Key");
         }
-
         return new ApiKeyAuthentication(apiKey, AuthorityUtils.NO_AUTHORITIES);
     }
-
 }
