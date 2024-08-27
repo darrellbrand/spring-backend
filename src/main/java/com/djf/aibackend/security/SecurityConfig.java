faff836b989f71ab76e560eb9d3f1659feee903f
dj <darrellbrand@gmail.com>
2024-08-26 22:12:49 -0700

    baller shit got it going

--- src/main/java/com/djf/aibackend/security/SecurityConfig.java
+++ src/main/java/com/djf/aibackend/security/SecurityConfig.java
@@ -43,16 +43,17 @@ public class SecurityConfig {
             auth.anyRequest().authenticated();
         }).httpBasic(Customizer.withDefaults()).build();
     }
+
     @Bean
     @Order(3)
     SecurityFilterChain h2ConsoleSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .securityMatcher(AntPathRequestMatcher.antMatcher("/h2-console/**"))
-                .authorizeHttpRequests( auth -> {
+                .authorizeHttpRequests(auth -> {
                     auth.requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll();
                 })
                 .csrf(csrf -> csrf.ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")))
-                .headers(headers -> headers.frameOptions().disable())
+                .headers(headers -> headers.frameOptions(Customizer.withDefaults()).disable())
                 .build();
     }
 
